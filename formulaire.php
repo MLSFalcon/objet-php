d732d5c8e521fff0dd8d07b678c81430cc71abce
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-01-22 09:30:56 +0100

    feat: connexion réussi

--- formulaire.php
+++ formulaire.php
@@ -1,15 +1,15 @@
 <?php
-require_once "Include/Bdd.php";
+require_once "traitement/Bdd.php";
 require_once "traitement/GestionUtilisateur.php";
 ?>
 <h2>Inscription</h2>
 <form method="post">
     <label>Nom
         <input type="text" name="nom"></label>
-    <label>Prenom
+    <label>Email
         <input type="text" name="email"></label>
-    <label>tel
-        <input type="text" name="mdp"></label>
+    <label>Mdp
+        <input type="password" name="mdp"></label>
     <input type="submit" name="ajoutUser">
 </form>
 <?php
@@ -17,14 +17,20 @@ if(isset($_POST['ajoutUser'])){
     $user = new GestionUtilisateur($_POST);
     $user->inscription();
 }
+if (isset($_GET['succes'])){
+    echo $_GET['succes'];
+}
+if (isset($_GET['erreur'])){
+    echo $_GET['erreur'];
+}
 ?>
 <br>
 <h2>Connexion</h2>
 <form method="post">
-    <label>Nom
+    <label>Email
         <input type="text" name="email"></label>
-    <label>Prenom
-        <input type="text" name="mdp"></label>
+    <label>mdp
+        <input type="password" name="mdp"></label>
     <input type="submit" name="connexion">
 </form>
 <br>
@@ -32,5 +38,17 @@ if(isset($_POST['ajoutUser'])){
 if(isset($_POST['connexion'])){
     $user = new GestionUtilisateur($_POST);
     $user->connexion();
+    session_start();
+    $_SESSION['email'] = $_POST['email'];
+}
+if (isset($_GET['succesConnexion'])){
+    echo $_GET['succesConnexion'];
+    session_start();
+    var_dump($_SESSION);
+}
+if (isset($_GET['erreurConnexion'])){
+    echo $_GET['erreurConnexion'];
+    session_start();
+    session_destroy();
 }
 ?>
--- traitement/GestionUtilisateur.php
+++ traitement/GestionUtilisateur.php
@@ -1,18 +1,16 @@
 <?php
-require_once "../Include/Bdd.php";
+require_once "Bdd.php";
 class GestionUtilisateur
 {
-private $nom;
-private $email;
-private $mdp;
+    private $nom;
+    private $email;
+    private $mdp;
     private function hydrate(array $donnees) {
         foreach ($donnees as $key => $value) {
             $method = 'set'.ucfirst($key);
 
             if (method_exists($this, $method)) {
                 $this->$method($value);
-            }else{
-                echo $key." - err";
             }
         }
     }
@@ -21,29 +19,51 @@ private $mdp;
         $this->hydrate($donnee);
     }
 
-public function inscription(){
-    $bdd = new Bdd();
-    $req = $bdd->getBdd()->prepare('INSERT INTO user(nom,email,mdp) VALUES (:nom,:email,:mdp)');
-    $req->execute(array(
-        'nom' => $this->nom,
-        'email' => $this->email,
-        'mdp' => $this->mdp
-    ));
-}
-public function connexion(){
+    public function inscription(){
+        $bdd = new Bdd();
+        $req = $bdd->getBdd()->prepare('SELECT * FROM user WHERE email = :email');
+        $req->execute(array(
+            'email' => $this->email
+        ));
+        $liste = $req->fetchAll();
+        if ($liste){
+            header("location:register.php?erreur=Le compte existe déjà");
+        } else {
+            $mdpchiffre = password_hash($this->mdp, PASSWORD_DEFAULT);
+            $req = $bdd->getBdd()->prepare('INSERT INTO user(nom,email,mdp) VALUES (:nom,:email,:mdp)');
+            $req->execute(array(
+                'nom' => $this->nom,
+                'email' => $this->email,
+                'mdp' => $mdpchiffre
+            ));
+            header("location: formulaire.php?succes=Le compte a bien été ajouté");
+        }
+    }
+    public function connexion(){
+        $bdd = new Bdd();
+        $req = $bdd->getBdd()->prepare('SELECT * FROM user WHERE email = :email');
+        $req->execute(array(
+            'email' => $this->email,
+        ));
+        $liste = $req->fetch();
 
-}
+        if ($liste && password_verify($this->mdp, $liste['mdp'])){
+            header("location: formulaire.php?succesConnexion=Connexion réussi");
+        }else{
+            header("location: formulaire.php?erreurConnexion=Le compte n'existe pas");
+        }
+    }
     public function setNom($nom)
     {
         $this->nom = $nom;
     }
-    public function setEmail($prenom)
+    public function setEmail($email)
     {
-        $this->prenom = $prenom;
+        $this->email = $email;
     }
-    public function setMdp($tel)
+    public function setMdp($mdp)
     {
-        $this->tel = $tel;
+        $this->mdp = $mdp;
     }
 
 }
